b177343c83f6f93033f05b381214ccbfad10ca97
Madison Zeiger <addiebellzeiger@gmail.com>
2024-06-06 11:27:52 -0500

    Completed JS intro and variables exercise

--- app.js
+++ app.js
@@ -3,8 +3,46 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1 & 2
+const lastName = "Zeiger";
+const firstName = "Madison";
+var age = 29
+console.log(lastName)
+console.log(firstName)
+console.log(age)
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+console.log (language)
+console.log(createdYear)
+console.log(isCaseSensitive)
+
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M"
+console.log(price)
+console.log(isOnSale)
+console.log(salePercentage)
+console.log(stock)
+console.log(inStock)
+console.log(selectedSize)
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(title)
+console.log(author)
+console.log(pageCount)
+console.log(bookmark)
+console.log(hasRead)
+ 
